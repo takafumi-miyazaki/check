e371ad14d2e5cf47f5ebade16a3ab0ebfbb091b8
takafumi-miyazaki <ttttfumi.miyazzzz@gmail.com>
2019-08-14 17:47:36 +0900

    modify glibcVersionCheck.c and Makefile

---
+++ src/glibcVersionCheck.c
@@ -0,0 +1,14 @@
+/**
+ * glibcVersionCheck.c
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <gnu/libc-version.h>
+
+int main(int argc, char *argv[])
+{
+  printf("GNU libc version: %s\n", gnu_get_libc_version());
+  exit(EXIT_SUCCESS);
+}
